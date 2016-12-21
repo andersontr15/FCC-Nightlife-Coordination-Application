cfe3de1809a8a47713306c4acc472630f109ade1
Theo Anderson <theoanderson3@gmail.com>
2016-12-21 15:45:19 -0800

    first push

---
+++ routes/api.js
@@ -0,0 +1,68 @@
+let express = require('express');
+let jwt = require('jsonwebtoken');
+let bcrypt = require('bcrypt-nodejs');
+let router = express.Router();
+let User = require('../models/user');
+let Yelp = require('yelp');
+
+router.get('/location/:zip', (request, response) => {
+	var yelp = new Yelp({
+	  	consumer_key: 'es3aQv39bBet81yzmDY2wA',
+	  	consumer_secret: 'OnuNy18XbW5EumNjvxwlHVu6j1Q',
+	  	token: 'QhAuNYcn7vFtnwv_77u3uk59czehMyRy',
+	  	token_secret: '4kQgtt26rUtTKUt9QNgBAl4s1CA'
+	});
+
+	yelp.search({ term: 'bars', location: request.params.zip })
+		.then(function (data) {
+		  return response.status(200).send(data);
+		})
+		.catch(function (err) {
+		  return response.status(400).send(err);
+		});
+});
+
+
+router.post('/login', (request, response) => {
+	console.log(request.body);
+	if(request.body.name && request.body.password) {
+		User.findOne({ name:  request.body.name }, (err, user) => {
+			if(err) {
+				return response.status(400).send(err)
+			}
+			if(!user) {
+				return response.status(404).send('No user with this account is registered');
+			}
+			var token = jwt.sign({
+				data: user
+			}, process.env.secret, { expiresIn: 3600 });
+			return response.status(200).send(token);
+		});
+	}
+	else {
+		return response.status(400).send('Please fill out all fields');
+	}
+});
+
+router.post('/register', (request, response) => {
+	if(request.body.name && request.body.password && request.body.location) {
+		var user = new User();
+		user.name = request.body.name;
+		user.password = bcrypt.hashSync(request.body.password, bcrypt.genSaltSync(10));
+		user.location = request.body.location;
+		user.save((err, resource) => {
+			if(err) {
+				return response.status(400).send(err);
+			}
+			var token = jwt.sign({
+				data: resource
+			}, process.env.secret, { expiresIn: 3600});
+			return response.status(201).send(token);
+		});
+	}
+	else {
+		return response.status(400).send('Must fill out all fields');
+	};
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,42 @@
+const PORT = process.env.PORT || 3000;
+const db = "mongodb://localhost:27017/fcc-nightlife";
+let express = require('express');
+let mongoose = require('mongoose');
+let bodyParser = require('body-parser');
+let morgan = require('morgan');
+let dotenv = require('dotenv');
+let router = require('./routes/api');
+dotenv.config({ verbose: true });
+
+mongoose.connect(db, (err) => {
+	if(err) {
+		console.log(err)
+	}
+});
+
+mongoose.connection.on('connected', () => console.log('Mongoose has connected to ' + db));
+
+mongoose.connection.on('disconnected', () => console.log('Mongoose has disconnected from ' + db));
+
+mongoose.connection.on('error', () => console.log('An error has occured connecting to ' + db));
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.use('/api', router);
+
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+app.get('*', (request, response) => {
+	response.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(PORT, () => console.log('Listening on port ' + PORT));
+
+
